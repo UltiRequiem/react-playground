0c370c85b6632ebb873464fc6b3fd502c10e9357
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-11 07:56:25 -0500

    :art: Update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-// Uncomment to see the project!
+/* eslint-disable import/no-dynamic-require */
 
-// import loadApp from './proyects/CounterButton'
-import loadApp from './proyects/InputObserver'
+import React, { Suspense, lazy } from 'react'
+import ReactDOM from 'react-dom'
 
-if (typeof loadApp === 'function') {
-  loadApp()
-} else {
-  console.log("You haven't imported any application yet!")
+const APPS = {
+  counterButton: 'CounterButton',
+  inputObserver: 'InputObserver',
 }
+
+const App = lazy(() => import(`./proyects/${APPS.counterButton}`))
+
+ReactDOM.render(
+  <Suspense fallback={<div>Loading...</div>}>
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+)
--- proyects/InputObserver/index.js
+++ proyects/InputObserver/index.js
@@ -1,9 +1,28 @@
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
 
-import App from './app'
+import { Label, Input, GrettingLayout, H1, P } from './styles'
 
-function loadApp() {
-  ReactDOM.render(<App />, document.getElementById('root'))
+const App = () => {
+  const [name, setName] = useState(null)
+
+  return (
+    <>
+      <H1>Hello{name ? ` ${name}` : ''}!</H1>
+
+      <form>
+        <GrettingLayout>
+          <Label htmlFor="name">Input your name:</Label>
+          <Input
+            type="text"
+            id="name"
+            onChange={(event) => setName(event.currentTarget.value)}
+          />
+        </GrettingLayout>
+      </form>
+
+      {name && <P>Thanks for fill out the survey!</P>}
+    </>
+  )
 }
 
-export default loadApp
+export default App
